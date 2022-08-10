f998753ea63c96de18b6548ee6a6aa820002e3f6
po1nt <po1nt.sploit@gmail.com>
2022-08-10 23:39:34 +1000

    sh name fix

--- chromedriver_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-version="$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE_103 2>/dev/null)" && \
-rm chromedriver* 2> /dev/null 1>&2 ; \
-wget "https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip" 2> /dev/null 1>&2 && \
-unzip chromedriver_linux64.zip 2> /dev/null 1>&2 && \
-rm chromedriver_linux64.zip* 2> /dev/null 1>&2
