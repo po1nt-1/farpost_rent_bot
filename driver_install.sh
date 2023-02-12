b0d0b7dbab7ddfc8a0c49c2fffe97a49a7f1d454
po1nt <po1nt.sploit@gmail.com>
2023-02-13 02:52:04 +1000

    driver install script updated

--- driver_install.sh
+++ driver_install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-version="$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE_103 2>/dev/null)" && \
-rm chromedriver* 2> /dev/null 1>&2 ; \
-wget "https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip" 2> /dev/null 1>&2 && \
-unzip chromedriver_linux64.zip 2> /dev/null 1>&2 && \
-rm chromedriver_linux64.zip* 2> /dev/null 1>&2
+set -e # -x
+
+if command -v yum >/dev/null; then
+    sudo yum install -qy https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+elif command -v apt >/dev/null; then
+    wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable_current_amd64.deb && \
+    sudo apt install ./google-chrome-stable_current_amd64.deb && \
+    rm google-chrome-stable_current_amd64.deb
+else
+  echo "You need to install google-chrome"; exit -1
+fi
+
+google-chrome --version
+
+version="$(google-chrome --version | cut -d' ' -f3)" && \
+wget -q "https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip" -O chromedriver_linux64.zip && \
+unzip -q -o chromedriver_linux64.zip chromedriver && \
+rm -f chromedriver_linux64.zip* && \
+./chromedriver --version | cut -d' ' -f1,2
